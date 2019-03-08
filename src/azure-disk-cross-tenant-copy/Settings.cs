e456107356c7b00a345223a0a1fd280fdfc588bf
Dusch <sebastian.dusch@sap.com>
2019-03-08 12:03:19 +0100

    Add Settings class

---
+++ src/azure-disk-cross-tenant-copy/Settings.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace azure_disk_cross_tenant_copy
+{
+    public class Settings
+    {
+        public static string sourceTenantID = String.Empty; //TODO: replace with own
+        public static string sourceClientID = String.Empty; //TODO: replace with own
+        public static string sourceClientSecret = String.Empty; //TODO: replace with own
+        public static string sourceDiskId = String.Empty; //TODO: replace with own
+        public static string targetTenantID = String.Empty; //TODO: replace with own
+        public static string targetClientID = String.Empty; //TODO: replace with own
+        public static string targetClientSecret = String.Empty; //TODO: replace with own
+        public static string targetDiskId = String.Empty; //TODO: replace with own
+    }
+}
